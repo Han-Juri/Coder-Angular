3aefb35bef31f4a3ac15585ab73c195e4c4b059a
Franco J Beccari <francojoaquinbeccari@gmail.com>
2023-06-22 12:24:53 -0300

    componentes Beccari

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { MainContainerComponent } from './Components/main-container/main-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    NavbarComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule,
